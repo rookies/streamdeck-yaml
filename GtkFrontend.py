8c7f83d9cad5962b0a6c80fd1d169d2b3ba7e2c8
Robert Knauer <robert@capsaicin-dev.de>
2022-02-19 12:53:58 +0100

    Initialize repository

---
+++ GtkFrontend.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+
+class GtkFrontend:
+    def __init__(self, rows, columns, callback):
+        self._layout = (rows, columns)
+        self._callback = callback
+
+        self._window = Gtk.Window()
+        self._window.connect("destroy", Gtk.main_quit)
+
+        self._grid = Gtk.Grid()
+        self._grid.set_row_homogeneous(True)
+        self._grid.set_column_homogeneous(True)
+        self._window.add(self._grid)
+
+        self._buttons = []
+        for row in range(rows):
+            for col in range(columns):
+                button = Gtk.Button()
+
+                key_index = self.key_index(row, col)
+                button.connect("clicked", self._keypress, key_index)
+
+                self._buttons.append(button)
+                self._grid.attach(button, col, row, 1, 1)
+
+    def clear(self):
+        for button in self._buttons:
+            button.set_label("")
+            button.set_image(None)
+
+    def draw(self):
+        self._window.show_all()
+
+    def run(self):
+        Gtk.main()
+
+    def set_key(self, key_index, title, image_path):
+        image = Gtk.Image.new_from_file(image_path)
+        self._buttons[key_index].set_label(title)
+        self._buttons[key_index].set_always_show_image(True)
+        self._buttons[key_index].set_image_position(Gtk.PositionType.TOP)
+        self._buttons[key_index].set_image(image)
+
+    def key_index(self, row, col):
+        return row * self._layout[1] + col
+
+    def _keypress(self, button, key_index):
+        self._callback(key_index)
---
+++ demo.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+import sys
+import os.path
+import yaml
+from GtkFrontend import GtkFrontend
+
+
+class Main:
+    def __init__(self, argv):
+        if len(argv) != 2:
+            print(f"Usage: {argv[0]} layout.yml")
+            sys.exit(1)
+
+        with open(argv[1]) as f:
+            self.layout = yaml.safe_load(f)
+
+        self._submenu = []
+        self._icon_path = os.path.join(os.path.dirname(__file__), "icons")
+        self._frontend = GtkFrontend(self.layout["rows"], self.layout["columns"], self._callback)
+
+    def run(self):
+        self._update()
+        self._frontend.run()
+
+    def _update(self):
+        submenu_layout = self.get_submenu_layout()
+
+        self._frontend.clear()
+        for row in range(self.layout["rows"]):
+            for col in range(self.layout["columns"]):
+                key_index = self._frontend.key_index(row, col)
+
+                if key_index >= len(submenu_layout):
+                    break
+                key_config = submenu_layout[key_index]
+                if key_config is None:
+                    continue
+
+                self._frontend.set_key(key_index, key_config["title"], os.path.join(self._icon_path, f"{key_config['icon']}.png"))
+        self._frontend.draw()
+
+    def _callback(self, key_index):
+        submenu_layout = self.get_submenu_layout()
+
+        if key_index >= len(submenu_layout):
+            return
+        key_config = submenu_layout[key_index]
+        if key_config is None:
+            return
+
+        if key_config["kind"] == "SubMenu":
+            self._submenu.append(key_index)
+            self._update()
+        elif key_config["kind"] == "BackButton":
+            self._submenu.pop()
+            self._update()
+        else:
+            # TODO
+            print(key_index, key_config)
+
+    def get_submenu_layout(self):
+        submenu_layout = self.layout["keys"]
+        for i in self._submenu:
+            submenu_layout = submenu_layout[i]["values"]["keys"]
+
+        return submenu_layout
+
+
+if __name__ == '__main__':
+    Main(sys.argv).run()
